6a60919ed6b4678fd7b5904e1def690fae0085e6
nachobertero <130572058+nachobertero@users.noreply.github.com>
2024-04-19 16:25:41 -0300

    Update para-busca-carrera.js

--- public/js/para-busca-carrera.js
+++ public/js/para-busca-carrera.js
@@ -1,4 +1,6 @@
-let carreraAmostrar = null;
+// PRIMER VERSIÓN
+
+/* let carreraAmostrar = null;
 window.onload = recuperarCarrera();
 
 //recupera lo guardado por index.html
@@ -11,4 +13,20 @@ let seccionCarreras = document.querySelector(".provisoria");
 
 seccionCarreras.innerHTML = `
     <p>La carrera elegida en el buscador es ${carreraAmostrar} papáaa</p>
-`;
+`; */
+
+// SEGUNDA VERSIÓN
+
+let carreraAmostrar = null;
+window.onload = recuperarCarrera();
+
+function recuperarCarrera() {
+    carreraAmostrar = sessionStorage.getItem("carrera");
+    
+
+    const tituloh1 = document.querySelector(".design5");
+
+    tituloh1.innerHTML = `
+        <p>Carreras de ${carreraAmostrar}</p>
+    `;
+}
